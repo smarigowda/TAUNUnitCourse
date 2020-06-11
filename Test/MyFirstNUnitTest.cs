9e4316466e3a8359a0e704ccf0c26e583d5e890f
Santosh Marigowda <santosharakere@gmail.com>
2020-06-11 11:42:17 +0100

    added an async test

--- Test/MyFirstNUnitTest.cs
+++ Test/MyFirstNUnitTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Test
@@ -10,7 +11,18 @@ namespace Test
         {
             Console.WriteLine("will see this in console");
             Assert.True(true);
-        }
 
+        }
+        [Test]
+        public static void ThisIsAStaticTest()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public async Task IAmAnAsyncTest()
+        {
+            Assert.True(true);
+            await Task.CompletedTask;
+        }
     }
 }
