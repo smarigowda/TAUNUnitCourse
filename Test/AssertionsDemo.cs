e56dd9197f3eef56976b75b91a24eb8c0da738ae
Santosh Marigowda <santosharakere@gmail.com>
2020-06-11 15:46:39 +0100

    reference equality

---
+++ Test/AssertionsDemo.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class AssertionsDemo
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AreEqual()
+        {
+            Assert.AreEqual("expected", "actual");
+        }
+        [Test]
+        public void AreEqualWithDescription()
+        {
+            Assert.AreEqual("expected", "actual", "test failed, give some context here...");
+        }
+        [Test]
+        public void AreEqualArrays()
+        {
+            var expected = new int[] { 1, 2, 3, 4, 5 };
+            var actual = new int[] { 1, 2, 3, 7, 8, 9 };
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AreObjectsEqual()
+        {
+            var obj1 = new object();
+            var obj2 = new object();
+            var areTheyEqual = obj1.Equals(obj2); // reference equaliy, FAILs
+            // Assert.IsTrue(areTheyEqual);
+            // Assert.AreEqual(obj1, obj2); // reference equality, FAILs
+            obj2 = obj1;
+            Assert.AreEqual(obj1, obj2); // PASSes
+        }
+    }
+}
