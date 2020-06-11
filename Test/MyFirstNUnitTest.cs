4984f9f03186f40ad3a07d5471585de9eb90b5a1
Santosh Marigowda <santosharakere@gmail.com>
2020-06-11 11:03:55 +0100

    first test

---
+++ Test/MyFirstNUnitTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    public class MyFirstNUnitTest
+    {
+        [Test]
+        public void IAmDoningGreat()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
