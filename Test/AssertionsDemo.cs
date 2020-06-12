d9cdd8ac3e48b5d9815131288150b8f73509efd1
Santosh Marigowda <santosharakere@gmail.com>
2020-06-12 10:56:00 +0100

    using multiple assertions

--- Test/AssertionsDemo.cs
+++ Test/AssertionsDemo.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Tests
@@ -48,19 +49,43 @@ namespace Tests
         [Test]
         public void ContainsOneInstanceOfThree()
         {
-            var arrayOfValues = new int[] { 1, 3, 5, 6,7};
+            var arrayOfValues = new int[] { 1, 3, 5, 6, 7 };
             Assert.That(arrayOfValues, Has.Exactly(1).EqualTo(3));
             Assert.That(arrayOfValues, Has.One.EqualTo(6));
         }
         [Test]
-        public void IsMoreThan5AndLessThan50() {
+        public void IsMoreThan5AndLessThan50()
+        {
             var num = 45;
             Assert.That(num, Is.GreaterThan(5).And.LessThan(100));
         }
         [Test]
-        public void UsingWarning() {
+        public void UsingWarning()
+        {
             var isProcessed = false;
             Warn.Unless(isProcessed, Is.True.After(1).Minutes.PollEvery(10).Seconds);
+            Console.WriteLine("Still going on...");
+        }
+        [Test]
+        public void AssertPassThrowsException()
+        {
+            Assert.That(Assert.Pass, Throws.Exception);
+            // Assert.That(Assert.Pass, Throws.TypeOf<Exception>()); // fails
+            Assert.That(Assert.Pass, Throws.TypeOf<SuccessException>());
+        }
+        [Test]
+        public void AssumeDemo()
+        {
+            Assume.That("some value", Is.EqualTo("some other value")); // does not fail but just warns
+        }
+        [Test]
+        public void MultipleAssertions()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(1, Is.EqualTo(2));
+                Assert.That(4, Is.EqualTo(5));
+            });
         }
     }
 }
